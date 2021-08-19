8b1c92591167f443820bfb37b756f0a5c55e50f2
Carlos Arce <carlos.3101a@gmail.com>
2021-08-19 00:34:29 -0400

    adding calculated formulas

---
+++ js/page-one.js
@@ -0,0 +1,13 @@
+// Cuadrado
+const perimetroCuadrado = (lado) => lado * 4;
+const areaCuadrado = (lado) => lado * lado;
+
+// Triángulo
+const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
+const areaTriangulo = (base, altura) => (base * altura) / 2;
+
+// Circulo
+const diametroCirculo = (radio) => radio * 2;
+const PI = Math.PI;
+const perimetroCirculo = (radio) => diametroCirculo(radio) * PI;
+const areaCirculo = (radio) => PI * radio ** 2;
